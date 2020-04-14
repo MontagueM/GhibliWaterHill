d467f3fbc19b3dd998868c8e38c3cb98e46eb056
MontagueM <msm19@ic.ac.uk>
2020-04-15 00:58:08 +0100

    Init commit

---
+++ Source/GhibliWaterHill/GhibliWaterHillGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GhibliWaterHillGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GHIBLIWATERHILL_API AGhibliWaterHillGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
